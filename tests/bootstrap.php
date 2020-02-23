db45cf99f69f5066d1a48d500b6b3d70639b23c0
Yevgeny Tomenko <skie@mail.ru>
2020-02-23 23:27:47 +0300

    update dependencies

--- tests/bootstrap.php
+++ tests/bootstrap.php
@@ -16,8 +16,11 @@
  * Add additional configuration/setup your application needs when running
  * unit tests in this file.
  */
+use Cake\Datasource\ConnectionManager;
+
 require dirname(__DIR__) . '/vendor/autoload.php';
 
 require dirname(__DIR__) . '/config/bootstrap.php';
 
 $_SERVER['PHP_SELF'] = '/';
+
